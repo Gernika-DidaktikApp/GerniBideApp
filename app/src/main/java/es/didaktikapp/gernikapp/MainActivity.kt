ebab96ab15aa29efb68577a4dd16ead64d4b097b
arantxa <149501497+arantxaMain@users.noreply.github.com>
2026-01-05 19:13:30 +0100

    CHORE: añadir botones de atajo en pantalla de inicio

--- app/src/main/java/es/didaktikapp/gernikapp/MainActivity.kt
+++ app/src/main/java/es/didaktikapp/gernikapp/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import es.didaktikapp.gernikapp.plazagernika.PlazaVideoActivity
+import es.didaktikapp.gernikapp.plazagernika.ArrastrProductosActivity
+import es.didaktikapp.gernikapp.plazagernika.VersoGameActivity
+import es.didaktikapp.gernikapp.plazagernika.FotoMisionActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +15,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnPlazaGernika = findViewById<Button>(R.id.btnPlazaGernika)
-        btnPlazaGernika.setOnClickListener {
+        val btnVideo = findViewById<Button>(R.id.btnVideo)
+        btnVideo.setOnClickListener {
             val intent = Intent(this, PlazaVideoActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnMercado = findViewById<Button>(R.id.btnMercado)
+        btnMercado.setOnClickListener {
+            val intent = Intent(this, ArrastrProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnVersos = findViewById<Button>(R.id.btnVersos)
+        btnVersos.setOnClickListener {
+            val intent = Intent(this, VersoGameActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnFotos = findViewById<Button>(R.id.btnFotos)
+        btnFotos.setOnClickListener {
+            val intent = Intent(this, FotoMisionActivity::class.java)
             startActivity(intent)
         }
     }
