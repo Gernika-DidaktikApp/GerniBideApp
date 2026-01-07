52129dddba0791f65dc11d68eb06dca2d741c49d
Erlantz50 <erlantzgarcia05@gmail.com>
2026-01-07 20:38:57 +0100

    FEAT: Ajustes y importaciones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.flexbox)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.maps)
     testImplementation(libs.junit)
