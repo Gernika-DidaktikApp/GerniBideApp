c1dd7232def3186d5f349a1b206017e0d1c4c728
telcaslor <thatbytc@gmail.com>
2026-01-09 09:19:53 +0100

    FEAT: Primera version arbol

--- app/src/main/java/es/didaktikapp/gernikapp/MainActivity.kt
+++ app/src/main/java/es/didaktikapp/gernikapp/MainActivity.kt
@@ -1,12 +1,33 @@
 package es.didaktikapp.gernikapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Clear tree and mural values when the application opens
+        getSharedPreferences("CollectiveTreePrefs", MODE_PRIVATE)
+            .edit().clear().apply()
+        getSharedPreferences("PeaceMuralPrefs", MODE_PRIVATE)
+            .edit().clear().apply()
+
+        val btnArbol = findViewById<Button>(R.id.btnArbol)
+        val btnBunkers = findViewById<Button>(R.id.btnBunkers)
+
+        btnArbol.setOnClickListener {
+            val intent = Intent(this, ArbolActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBunkers.setOnClickListener {
+            val intent = Intent(this, SoundGameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
