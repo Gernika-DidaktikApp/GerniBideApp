30152ceb7dab87d952b71053f053292a2ba202fd
Warita <warayasyp@gmail.com>
2026-01-25 21:54:37 +0100

    ui: añadir perfil a menu principal

--- app/src/main/java/es/didaktikapp/gernikapp/BaseMenuActivity.kt
+++ app/src/main/java/es/didaktikapp/gernikapp/BaseMenuActivity.kt
@@ -8,7 +8,6 @@ import android.view.animation.AccelerateInterpolator
 import android.view.animation.OvershootInterpolator
 import android.widget.FrameLayout
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 
@@ -100,7 +99,7 @@ abstract class BaseMenuActivity : AppCompatActivity() {
 
         fabProfile.setOnClickListener {
             closeMenu()
-            Toast.makeText(this, getString(R.string.fab_profile), Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, ProfileActivity::class.java))
         }
     }
 
